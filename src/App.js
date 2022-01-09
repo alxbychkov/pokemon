e7f96374e7759be56a65697afed41ca4b33200d0
Alexander Bychkov <alx.bychkov@gmail.com>
2022-01-09 22:06:19 +0300

    start project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header/Header.js';
+import Layout from './components/Layout/Layout.js';
+import Footer from './components/Footer/Footer.js';
+import bg1 from './assets/img/bg1.jpg';
+import bg2 from './assets/img/bg2.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header 
+        title="This is title"
+        desc="This is description!"  
+      />
+      <Layout
+        id="rules"
+        title="First layout"
+        desc="First desc"
+        urlBg={bg1}
+      />
+      <Layout
+        id="cards"
+        title="Second layout"
+        desc="Second desc"
+        colorBg='#e2e2e2'
+      />
+      <Layout
+        id="about"
+        title="Third layout"
+        desc="Third desc"
+        urlBg={bg2}
+      />
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,31 @@
+import style from './style.module.css';
+
+function Layout({id, title, desc, urlBg, colorBg}) {
+    const sectionStyle = {};
+    
+    if (urlBg) {
+        sectionStyle.backgroundImage = `url(${urlBg})`;
+    }
+
+    if (colorBg) {
+        sectionStyle.backgroundColor = colorBg;
+    }
+
+    return (
+        <section className={style.root} style={sectionStyle} id={id}>
+            <div className={style.wrapper}>
+                <article>
+                    <div className={style.title}>
+                        <h3>{title}</h3>
+                        <span className={style.separator}></span>
+                    </div>
+                    <div className={[style.desc, style.full]}>
+                        <p>{desc}</p>
+                    </div>
+                </article>
+            </div>
+        </section>
+    );
+}
+
+export default Layout;
